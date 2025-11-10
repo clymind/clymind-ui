48dbcd3bdaba1cad72b3d7c60f06256e0f6db70f
Emanuele <emanuele.barbieri97@gmail.com>
2025-11-10 19:49:04 +0100

    create the basic files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import Message from "./Message";
-
 function App() {
   return (
     <div>
       <h1>Welcome to the Startups Website!</h1>
-      <Message />
     </div>
   );
 }
--- src/Message.tsx
+++
@@ -1,6 +0,0 @@
-function Message() {
-    var name = "Ema";
-    return <div>Hello {name}!</div>;
-}
-
-export default Message;
