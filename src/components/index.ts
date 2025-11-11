32d55059dae5cedd3f079744837791c67417444d
Emanuele <emanuele.barbieri97@gmail.com>
2025-11-11 16:35:44 +0100

    Revert "Dev"

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { PageShell } from "./layout/PageShell";
-export { Container } from "./layout/Container";
-export { InfoStrip } from "./ui/InfoStrip";
-export { SearchBar } from "./ui/SearchBar";
-export { Tabs } from "./ui/Tabs";
-export { FloatingRefresh } from "./ui/FloatingRefresh";
-export { StartupCard } from "./StartupCard";
-export { LeaderboardRow } from "./LeaderboardRow";
--- src/components/layout/Container.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Container = ({ className = "", children }: { className?: string; children: React.ReactNode }) => (
-  <div className={`container ${className}`}>{children}</div>
-);
